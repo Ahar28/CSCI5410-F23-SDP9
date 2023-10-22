782b243b2c467902116635dfd5e677306178d53b
Bhargav <bh950065@dal.ca>
2023-10-21 21:01:51 -0300

    get all restaurant,restaurant by id, get menu and get timings apis added

---
+++ Customer App/Backend/restaurants.js
@@ -0,0 +1,110 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+    region:'us-east-1'
+})
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const resTab = 'restaurant_details';
+const restaurantPath = '/restaurant';
+const restaurantsPath = '/restaurants';
+const menuPath = restaurantPath+'/menu';
+const availabilityPath = restaurantPath+'/availabletime';
+exports.handler = async function(event,context) {
+    let response;
+    switch(true){
+        case event.context["http-method"]=='GET' && event.context["resource-path"]==restaurantPath:
+            response = await getRestaurant(event.params.querystring.restaurantId);
+            break;
+        case event.context["http-method"]=='GET' && event.context["resource-path"]==restaurantsPath:
+            response = await getRestaurants();
+            break;
+        case event.context["http-method"]=='GET' && event.context["resource-path"]==menuPath:
+            response = await getMenu(event.params.querystring.restaurantId);
+            break;
+        case event.context["http-method"]=='GET' && event.context["resource-path"]==availabilityPath:
+            response = await getAvailability(event.params.querystring.restaurantId);
+            break;
+    }
+    return response;
+};
+async function getRestaurant(restaurantId){
+    try{
+    const params = {
+        TableName: resTab,
+        Key: {
+            'restaurant_id':restaurantId
+        }
+    };
+    const data = await dynamoDB.get(params).promise();
+    return {
+        statusCode:200,
+        body:JSON.stringify(data)
+    };
+    } catch(error){
+        return { error };
+    }
+}
+
+async function getRestaurants(){
+    try{
+    const params={
+        TableName: resTab
+    };
+    const data = await scanDynamoRecords(params,[]);
+    return {
+        statusCode:200,
+        body:JSON.stringify(data)
+    };
+    } catch(error){
+        return {error};
+    }
+}
+async function scanDynamoRecords(scanParams,itemArray){
+    try{
+        const dynamoRecords = await dynamoDB.scan(scanParams).promise();
+        itemArray=itemArray.concat(dynamoRecords.Items);
+        if(dynamoRecords.lastEvaluatedKey){
+            scanParams.ExclusiveStartKey=dynamoRecords.lastEvaluatedKey;
+            return await scanDynamoRecords(scanParams,itemArray);
+        }
+        return itemArray;
+    } catch(error){
+        return {error};
+    }
+}
+async function getMenu(restaurantId){
+    try{
+    const params = {
+        TableName: resTab,
+        Key: {
+            'restaurant_id':restaurantId
+        }
+    };
+    const data = await dynamoDB.get(params).promise();
+    const menuData = data.Item.menu;
+    return {
+        statusCode:200,
+        body:JSON.stringify(menuData)
+    };
+    } catch(error){
+        return { error };
+    }
+}
+async function getAvailability(restaurantId){
+    try{
+    const params = {
+        TableName: resTab,
+        Key: {
+            'restaurant_id':restaurantId
+        }
+    };
+    const data = await dynamoDB.get(params).promise();
+    const availabilityData = data.Item.timings;
+    return {
+        statusCode:200,
+        body:JSON.stringify(availabilityData)
+    };
+    } catch(error){
+        return { error };
+    }
+}
