e4a937e700138068a3cec61f0dd009c631c16962
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2023-10-26 02:05:05 -0300

    updated comments

--- Customer App/Backend/crud operations on reservation/node/index.js
+++ Customer App/Backend/crud operations on reservation/node/index.js
@@ -1,46 +1,47 @@
-const admin = require('firebase-admin');
+  const admin = require('firebase-admin');
 
-// Initialize Firebase Admin SDK with your service account credentials
-const serviceAccount = require('./serviceAccountKey.json'); // Update with your file path
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://serverless-term-assignment.firebaseio.com', // Replace with your Firebase project URL
-});
+  // Initializing Firebase Admin SDK with service account credentials
+  const serviceAccount = require('./serviceAccountKey.json'); // file path for service account credentials
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://serverless-term-assignment.firebaseio.com', // Firebase project URL
+  });
 
-exports.handler = async (event, context) => {
-  try {
-    // Initialize Firestore
-    const db = admin.firestore();
-   
-    // Data to be added to the Firestore document
-    const dataToStore = {
-      user_id: event.user_id, // Replace with the actual data you want to store
-      restaurant_id: event.restaurant_id,
-      no_of_people: event.no_of_people,
-      //timestamp: new Date().toISOString(),
-      // Add other data fields as needed
-    };
-
-    // Reference to the Firestore collection
-    const collectionRef = db.collection('Customer-Reservation'); // Replace with your collection name
+  exports.handler = async (event, context) => {
+    try {
+      // Initialize Firestore
+      const db = admin.firestore();
     
-    // Add a new document with a generated ID
-    const docRef = await collectionRef.add(dataToStore);
+      // Data to be added to the Firestore document
+      const dataToStore = {
+        user_id: event.user_id, 
+        restaurant_id: event.restaurant_id,
+        no_of_people: event.no_of_people,
+        //timestamp: new Date().toISOString(),
+        //  other data fields to be added as needed
+      };
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Document added successfully',
-        document_id: docRef.id,
-      }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: 'Failed to add document',
-        message: error.message,
-      }),
-    };
-  }
-};
+      // Reference to the Firestore collection
+      const collectionRef = db.collection('Customer-Reservation'); // collection name
+      
+      // Add a new document with an auto generated ID
+      const docRef = await collectionRef.add(dataToStore);
+
+      // return message
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Document added successfully',
+          document_id: docRef.id,
+        }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: 'Failed to add document',
+          message: error.message,
+        }),
+      };
+    }
+  };
