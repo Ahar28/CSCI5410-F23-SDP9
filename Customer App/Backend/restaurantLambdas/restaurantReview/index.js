295780b5c77861888d7f719a61ad1149474f12e2
Bhargav <bh950065@dal.ca>
2023-12-02 13:58:18 -0400

    reviews storing moved to firebase

--- Customer App/Backend/restaurantLambdas/restaurantReview/index.js
+++ Customer App/Backend/restaurantLambdas/restaurantReview/index.js
@@ -1,29 +1,26 @@
 const AWS = require("aws-sdk");
-AWS.config.update({
-    region:'us-east-1'
-})
-
+const admin = require("firebase-admin");
+const axios = require("axios");
+const serviceAccount = require("./service-account.json"); 
+const resTab='restaurant_details'
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const resTab = 'restaurant_details';
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://serverless-term-assignment.firebaseio.com",
+});
 exports.handler = async (event) => {
   try{
         const body = event['body-json'];
+        const db = admin.firestore();
         const overallReviewdata = await getOverallReviews(body.restaurant_id);
-        const overRatingCount = overallReviewdata.restaurant_review_overall.total_numberratings+1; 
         const overRatingValue = overallReviewdata.restaurant_review_overall.total_ratingvalue+body.reviewObj.rating;
-        const existingReviews = overallReviewdata.restaurant_reviews || [];
-        existingReviews.push({
-            review: body.reviewObj.review,
-            rating: body.reviewObj.rating,
-            user_id: body.reviewObj.user_id
-        });
+        const overRatingCount = overallReviewdata.restaurant_review_overall.total_numberratings+1;
         const params = {
             TableName: resTab,
             Key: { restaurant_id: body.restaurant_id },
-            UpdateExpression: 'SET #restaurant_reviews = :updatedReviews,#restaurant_review_overall = :newOverallRating',
-            ExpressionAttributeNames: { '#restaurant_reviews': 'restaurant_reviews','#restaurant_review_overall':'restaurant_review_overall' },
+            UpdateExpression: 'SET #restaurant_review_overall = :newOverallRating',
+            ExpressionAttributeNames: { '#restaurant_review_overall':'restaurant_review_overall' },
             ExpressionAttributeValues: {
-                ':updatedReviews': existingReviews,
                 ':newOverallRating':{
                     total_ratingvalue:overRatingValue,
                     total_numberratings:overRatingCount
@@ -33,16 +30,22 @@ exports.handler = async (event) => {
         };
         
         const data = await dynamoDB.update(params).promise();
-        if(data){
+        const reviewDocs = db.collection("restaurant-reviews");
+        const addReview= await reviewDocs.add({
+            restaurantId:body.restaurant_id,
+            review:body.reviewObj.review,
+            rating:body.reviewObj.rating,
+            userId:body.reviewObj.user_id
+        })
+        if(data && addReview){
             return {
             statusCode: 200,
             body: JSON.stringify({
                 message: 'Review posted successfully'
             })}
-        }
-        
+        }     
     } catch(error){
-        
+        console.log(error)
         return { error };
     }
 };
@@ -55,12 +58,22 @@ async function getOverallReviews(restaurantId){
         }
     };
     const data = await dynamoDB.get(params).promise();
+    let resRevOverall
+    if(data.Item.restaurant_review_overall){
+        resRevOverall=data.Item.restaurant_review_overall
+    } else{
+        resRevOverall={
+            total_ratingvalue:0,
+            total_numberratings:0
+        };
+    }
     const returnData = {
-        restaurant_review_overall:data.Item.restaurant_review_overall,
-        restaurant_reviews:data.Item.restaurant_reviews
+        restaurant_name:data.Item.restaurant_name,
+        restaurant_review_overall:resRevOverall,
     }
     return returnData;
     } catch(error){
         return { error };
     }
 }
+
